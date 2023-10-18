ad08dc3f1eaba36de217c9cbef8f7093ab719ea0
trmi1016 <tran.mike1016@gmail.com>
2023-10-18 13:23:22 +0200

    Copyright raus

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 /**
  * Das Modul besteht aus der Funktion {@linkcode getLogger} für einen Logger auf
  * der Basis von Pino: https://getpino.io. Pino wird auch von Fastify genutzt.
