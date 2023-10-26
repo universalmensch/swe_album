7c899a1c47819f8d15857ab0cf6cb43041944e80
trmi1016 <tran.mike1016@gmail.com>
2023-10-26 16:33:25 +0200

    album entity

--- src/album/entity/album.entity.ts
+++ src/album/entity/album.entity.ts
@@ -1,8 +1,49 @@
-import { Kuenstler } from "./kuenstler.entity";
-import { Lied } from "./lied.entity";
+import {
+    Column,
+    Entity,
+    OneToMany,
+    OneToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Kuenstler } from './kuenstler.entity';
+import { Lied } from './lied.entity';
 
+export type Genre = 'POP' | 'RAP' | 'ROCK';
+
+@Entity()
 export class Album {
+    @Column('int')
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+
+    @Column('varchar', { length: 12 })
+    @ApiProperty({ example: 'POP', type: String })
+    readonly genre: Genre | undefined;
+
+    @OneToMany(() => Lied, (lied) => lied.album, {
+        cascade: ['insert', 'remove'],
+    })
+    readonly lieder: Lied[] | undefined;
+
+    @OneToOne(() => Kuenstler, (kuenstler) => kuenstler.album, {
+        cascade: ['insert', 'remove'],
+    })
     readonly kuenstler: Kuenstler | undefined;
 
-    readonly lieder: Lied[] | undefined;
+    @Column('varchar', { length: 40 })
+    readonly name: string | undefined;
+
+    @Column('varchar', { length: 40 })
+    readonly titelbild: string | undefined;
+
+    public toString = (): string =>
+        JSON.stringify({
+            id: this.id,
+            genre: this.genre,
+            lieder: this.lieder,
+            kuenstler: this.kuenstler,
+            name: this.name,
+            titelbild: this.titelbild,
+        });
 }
