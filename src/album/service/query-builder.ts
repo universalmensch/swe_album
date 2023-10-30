d64b20d01de019513088153729c30c2fa1c06f51
Melvin Grzalik <147506157+melvin1203@users.noreply.github.com>
2023-10-30 17:23:10 +0100

    Update query-builder.ts

--- src/album/service/query-builder.ts
+++ src/album/service/query-builder.ts
@@ -64,7 +64,7 @@ export class QueryBuilder {
     }
 
     /**
-     * Bücher asynchron suchen.
+     * Alben asynchron suchen.
      * @param suchkriterien JSON-Objekt mit Suchkriterien
      * @returns QueryBuilder
      */
