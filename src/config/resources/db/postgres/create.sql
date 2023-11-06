09e75e04684b39ad2625acca03b3a456869231e5
trmi1016 <tran.mike1016@gmail.com>
2023-11-06 12:29:11 +0100

    album!

--- src/config/resources/db/postgres/create.sql
+++ src/config/resources/db/postgres/create.sql
@@ -9,15 +9,15 @@ CREATE TABLE IF NOT EXISTS album (
     genre         genre,
     name          varchar(40),
     titelbild     varchar(64)
-) TABLESPACE buchspace;
+) TABLESPACE albumspace;
 
 CREATE TABLE IF NOT EXISTS kuenstler (
     id          integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE albumspace,
     name        varchar(40) NOT NULL,
     vorname     varchar(40),
     alter       integer NOT NULL,
-    album_id    integer NOT NULL UNIQUE USING INDEX TABLESPACE buchspace REFERENCES buch
-) TABLESPACE buchspace;
+    album_id    integer NOT NULL UNIQUE USING INDEX TABLESPACE albumspace REFERENCES album
+) TABLESPACE albumspace;
 
 
 CREATE TABLE IF NOT EXISTS lied (
@@ -25,5 +25,5 @@ CREATE TABLE IF NOT EXISTS lied (
     name            varchar(40) NOT NULL,
     dauerinsekunden integer NOT NULL,
     album_id        integer NOT NULL REFERENCES album
-) TABLESPACE buchspace;
+) TABLESPACE albumspace;
 CREATE INDEX IF NOT EXISTS lieder_album_id_idx ON lied(album_id) TABLESPACE albumspace;
