5795aca93c892ceb7e21820acbbd05c770f7d620
trmi1016 <tran.mike1016@gmail.com>
2023-11-24 12:15:35 +0100

    Regex Lied

--- src/album/rest/liedDTO.entity.ts
+++ src/album/rest/liedDTO.entity.ts
@@ -3,7 +3,7 @@ import { IsInt, Matches, MaxLength, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LiedDTO {
-    @Matches('^\\w*')
+    @Matches('^[a-zA-Z]+')
     @MaxLength(32)
     @ApiProperty({ example: 'Die Beschriftung', type: String })
     readonly name!: string;
